60ba6d3ff4a9fd7da4e4ccf4593de0fe27856a2a
ArslanAmeer <arslan.ameer@hotmail.com>
2019-02-15 18:18:59 +0500

    ✔🔗👍 Update: Add Default Routing to Custom Action ViIew

--- FilingOnGoogleDrive-API/App_Start/RouteConfig.cs
+++ FilingOnGoogleDrive-API/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace FilingOnGoogleDrive_API
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Home", action = "GetGoogleDriveFiles", id = UrlParameter.Optional }
             );
         }
     }
